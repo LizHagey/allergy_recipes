5cf46dd9d9fad99e3b41fa6103fe965f72b8d46c
LizHagey <craftyvinyl@gmail.com>
2017-05-08 11:36:03 -0700

    Add files via upload

---
+++ info.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!--
+Information page
+-->
+<html lang="en-us">
+    <head>
+        <title>Information Page</title>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/Modules/head.php'; ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta name="description" content="What is Gluten">
+        <meta name="description" content="Basic information about gluten">
+    </head>
+    <body id="home">
+        <?php include_once("analyticstracking.php") ?>
+        <div>
+            <header id="pageHeader">
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/Modules/header.php'; ?>
+            </header>
+            <nav>
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/Modules/navigation.php'; ?>           
+            </nav>
+            <main>
+                <h1>What is Gluten</h1>
+                <p>In order to cook Gluten Free it helps if you understand what gluten is.<br><br>
+
+                    Gluten is two proteins that are found in wheat and other related grains such as barley and rye. The proteins  which are gliadin ( a prolamin protein)  and glutenin (a glutelin protein) are found in the wheat endosperm or seed. 
+                    <br><br>The proteins are not found in the grass itself which is why you could still have wheat grass juice if you were careful to never get the grain or seed in the juice.<br><br>
+
+                    Gluten when it gets wet becomes elastic and stretchy while still being strong.  <br><br>
+                    Gluten's name came from the glue-like qualities it possesses. <br><br>
+                    When yeast is added the gases from the yeast are stopped by the gluten. This causes the dough to rise. <br><br>
+
+                    In essence gluten is what gives a light and fluffy feel to food. It is also what makes food chewy. <br><br>
+                    The amount of  water added and the amount of stirring is what causes more elasticity and chewiness in the food. For instance a cake does not need as much elasticity as bread so less water and kneading  would be done with a cake. A pie crust needs to flake and break apart fairly easy, so it needs even less water and stirring than cake.<br><br>
+
+                    Also it is important to understand that the wheat seed has starch in it and the starch helps ingredients bind together.<br><br>
+
+                    Now that you understand what gluten does you will be  able to imitate it better in cooking. <br><br>
+                    In order to bind  Gluten free flours most people use  some sort of Gum product mixed with the flour such as guar gum or xanthum gum. <br><br>
+                    Starches are also used in conjuction with the gum product for the binding effect. <br><br>
+                    Another suggestion is to replace the protein that is in the gluten by using eggs for  ½ cup of water or use part nut flour or bean flour.<br><br>
+
+                    <em>To replace the Gluten flour most of us adore and can no longer eat.  You need to have a combination of whole grain flour(s) (Rice, corn, millet etc.), A starch(s) (potato, tapioca, corn, etc.), A gum or binder (guar,  xanthum, etc.).  I hope this helps.</em><br><br>
+
+By Peggy Fehlman</p>
+                
+                 <br><br><br>
+                    <div class="bottomSuggesedRecipes">   
+                <h2>You might like:</h2>
+                
+        <figure>
+            <h4>Easy Enchiladas</h4>
+            <a href="easyEnchiladas.php"><img src="/media/easyEnchiladas.jpg" alt="Easy Enchiladas"></a>
+            <figcaption>Fast and easy gluten free enchiladas that everyone will love.</figcaption> 
+        </figure>
+        <figure>
+            <h4>Broiled Salmon</h4>
+            <a href="broiledSalmon.php"><img src="/media/salmon.jpg" alt="Broiled Salmon"></a>
+            <figcaption>Nutrition is packed into this easy gluten free meal.  </figcaption> 
+        </figure>
+                    </div>
+                
+            </main>
+           <!-- <aside>
+                <ul>
+                    <li><a href='pepperSteak.php'>Main Dish</a></li>
+                    <li><a href='proteinBalls.php'>Snacks</a></li>
+                    <li><a href="bread.php">Bread</a></li>
+                    <li><a href="noBakeCookies.php">Dessert</a></li>
+                    <li><a href="easyMeals.php">Easy</a></li>
+                </ul>
+            </aside>-->
+            <footer>
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/Modules/footer.php'; ?>               
+            </footer>
+        </div>   
+    </body>
+</html>
