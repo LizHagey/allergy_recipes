12777878966d4d0c759c98839011ccdc2f9358d8
LizHagey <craftyvinyl@gmail.com>
2017-05-08 11:33:39 -0700

    Add files via upload

---
+++ salmon/Modules/head.php
@@ -0,0 +1,7 @@
+
+<meta charset="UTF-8">
+        <meta name="author" content="Elizabeth Hagey">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/salmon/SalmonCSS/Normalize.css" type="text/css" rel="stylesheet" media="screen"/>
+        <link href="/salmon/SalmonCSS/SalmonStyle.css" type="text/css" rel="stylesheet" media="screen"/>
+        <link rel="icon" type="image/png" href="salmon/media/faviconSalmon.jpg" sizes="16x16">
