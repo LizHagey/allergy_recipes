870fafa127083e72c63ae6eef022c65615265b33
LizHagey <craftyvinyl@gmail.com>
2017-06-06 14:37:47 -0700

    Add files via upload

--- topallergyfreerecipes/modules/header.php
+++ topallergyfreerecipes/modules/header.php
@@ -10,6 +10,14 @@
 
 <!-- the body section -->
 <body>
-    <header><a href ="/index.php"><img src="/images/logoLight.png" alt="Logo"></a>
-        
+    <header>
+        <ul id="topNav">
+            <li><a href ="/index.php"><img src="/images/logoLight.png" alt="Logo"></a></li>
+            <li><a href="/index.php">Home</a></li>
+            <li><a href="/tips.php">Tips</a></li>
+            <li><a href="/aboutMe.php">About Me</a></li>
+            <li><a href="/info.php">Info</a></li>
+            <li><a href="/contact/contact.php">Contact</a></li>
+            
+        </ul>
     </header>
